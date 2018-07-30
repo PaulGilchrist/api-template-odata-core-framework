dd445f1321d7626df3b37fdf3bc291448c6b6533
Paul Gilchrist <paul.gilchrist@outlook.com>
2018-07-30 08:47:45 -0400

    First Commit

---
+++ OdataCoreTemplate/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OData.Edm;
+using Newtonsoft.Json;
+using NJsonSchema;
+using NSwag.AspNetCore;
+using OdataCoreTemplate.Models;
+
+namespace OdataCoreTemplate
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("ApiDb"));
+			services.AddMvc()
+				.AddJsonOptions(options => {
+					options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+					options.SerializerSettings.ContractResolver =
+						new Newtonsoft.Json.Serialization.DefaultContractResolver();
+				});
+			services.AddOData();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+			app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly, settings => {
+				settings.GeneratorSettings.Title="ODataCoreTemplate";
+				settings.GeneratorSettings.DefaultPropertyNameHandling = PropertyNameHandling.CamelCase;
+				settings.GeneratorSettings.Description="OData .Net Core OpenAPI Project Template";
+				settings.GeneratorSettings.IsAspNetCore = true;
+				// settings.GeneratorSettings.DefaultUrlTemplate.
+				// settings.GeneratorSettings.SerializerSettings.
+			});
+
+			app.UseMvc(b => {
+				b.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
+				b.MapODataServiceRoute("ODataRoute", "odata", GetEdmModel());
+				b.EnableDependencyInjection();
+			});
+
+		}
+		private static IEdmModel GetEdmModel() {
+			ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+			builder.Namespace = "ApiTemplate";
+			builder.ContainerName = "ApiTemplateContainer";
+			builder.EnableLowerCamelCase();
+			builder.EntitySet<User>("Users");
+			return builder.GetEdmModel();
+		}
+
+	}
+
+
+}
