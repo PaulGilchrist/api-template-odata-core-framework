0314782001b7a4a8d5c1acc157a58095f8041079
Paul Gilchrist <paul.gilchrist@outlook.com>
2018-08-10 14:48:49 -0400

    Added root page to redirect to Swagger

--- API/Models/Address.cs
+++ API/Models/Address.cs
@@ -6,23 +6,33 @@ namespace ODataCoreTemplate.Models {
         [Key]
         public int Id { get; set; }
 
+        [Range(0, 99999)]
+        [Display(Name = "Street Number")]
         [Required]
         public int StreetNumber { get; set; }
 
+        [Display(Name = "Street Name")]
         [Required]
+        [StringLength(100)]
         public string StreetName { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string City { get; set; }
 
         [Required]
+        [StringLength(20)]
         public string State { get; set; }
 
+        [Display(Name = "Zip Code")]
         [Required]
+        [StringLength(11)]
         public string ZipCode { get; set; }
 
+        [StringLength(50)]
         public string Name { get; set; }
 
+        [StringLength(20)]
         public string Suite { get; set; }
 
         public List<User> Users { get; set; } = new List<User>();
--- API/Models/User.cs
+++ API/Models/User.cs
@@ -6,15 +6,21 @@ namespace ODataCoreTemplate.Models {
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
 
+        [StringLength(150)] 
         public string Email { get; set; }
 
-        public string Phone { get; set; }
+        [StringLength(20)]
+         public string Phone { get; set; }
 
         public List<Address> Addresses { get; set; } = new List<Address>();
     }
