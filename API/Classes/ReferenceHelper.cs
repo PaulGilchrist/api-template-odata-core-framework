b18bfe207cada68b540adb506b49fd4ec11be1d1
Paul Gilchrist <paul.gilchrist@outlook.com>
2019-03-19 15:47:06 -0400

    Cleaned up ReferenceHelper.cs

--- API/Classes/ReferenceHelper.cs
+++ API/Classes/ReferenceHelper.cs
@@ -27,47 +27,5 @@ namespace API.Classes {
             }
         }
 
-        //public static TKey GetKeyFromUri<TKey>(HttpRequestMessage request, Uri uri) {
-        //    if (uri==null) {
-        //        throw new ArgumentNullException("uri");
-        //    }
-        //    var urlHelper = request.GetUrlHelper()??new UrlHelper(request);
-        //    var pathHandler = (IODataPathHandler)request.GetRequestContainer().GetService(typeof(IODataPathHandler));
-        //    string serviceRoot = urlHelper.CreateODataLink(request.ODataProperties().RouteName, pathHandler, new List<ODataPathSegment>());
-        //    var odataPath = pathHandler.Parse(serviceRoot, uri.OriginalString, request.GetRequestContainer());
-        //    var keySegment = odataPath.Segments.OfType<KeySegment>().FirstOrDefault();
-        //    if (keySegment==null) {
-        //        throw new InvalidOperationException("The link does not contain a key.");
-        //    }
-
-        //    var value = keySegment.Keys.FirstOrDefault().Value;
-        //    return (TKey)value;
-        //}
-
-        //public static Dictionary<string, int> GetNestedKeyFromUri<TKey>(HttpRequestMessage request, Uri uri) {
-        //    if (uri==null) {
-        //        throw new ArgumentNullException("uri");
-        //    }
-        //    var urlHelper = request.GetUrlHelper()??new UrlHelper(request);
-        //    var pathHandler = (IODataPathHandler)request.GetRequestContainer().GetService(typeof(IODataPathHandler));
-        //    string serviceRoot = urlHelper.CreateODataLink(
-        //        request.ODataProperties().RouteName,
-        //        pathHandler, new List<ODataPathSegment>());
-        //    var odataPath = pathHandler.Parse(serviceRoot, uri.OriginalString, request.GetRequestContainer());
-        //    var keySegment = odataPath.Segments.OfType<KeySegment>();
-        //    if (keySegment==null) {
-        //        throw new InvalidOperationException("The link does not contain a key.");
-        //    }
-        //    if (keySegment.Count()<=1) {
-        //        return default;
-        //    }
-        //    var keyValue = (int)keySegment.ToList()[1].Keys.FirstOrDefault().Value;
-        //    var identifier = keySegment.ToList()[1].Identifier;
-        //    var value = new Dictionary<string, int>();
-        //    value.Add(identifier, keyValue);
-        //    return value;
-        //}
-
-
     }
 }
